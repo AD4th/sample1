ccdd3092566dffc6169a38d3e722dbedd0f6bd70
낭승태 <nang@ad4th.com>
2018-12-20 18:13:05 +0900

    @add : 언젠간 가겠지. 푸르던 내 청춘.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Router , Route, Switch, Redirect} from 'react-router-dom';
 import history from './config/history';
 import LoginComponent from './components/login';
+import MapComponent from './components/map';
 
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
       <Router history={history}>
           <Switch>
               <Route exact path="/login" component={LoginComponent} />
+              <Route exact path="/map" component={MapComponent} />
               <Redirect to="/" />
           </Switch>
       </Router>
---
+++ src/components/map/index.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './style.css';
+
+const profileImg = {
+    backgroundImage:"url('https://i.pinimg.com/736x/2c/2c/60/2c2c60b20cb817a80afd381ae23dab05.jpg')"
+}
+class Index extends Component {
+    render() {
+        return(
+            <div class="map_wrap">
+                <aside>
+                    <div class="user_info">
+                        <span class="user_profile" style={profileImg}></span>
+                        <span class="user_name">알렉시스산체스네마냐비디치</span>
+                    </div>
+                    <div class="user_comment">
+                        <ul>
+                            <li><a href="#">asdasdasdasdasdasdasdasdasdasdasdasdasdasdasd</a></li>
+                            <li><a href="#">asdasdasdasdasdasdasdasdasdasdasdasdasdasdasd</a></li>
+                            <li><a href="#">asdasdasdasdasdasdasdasdasdasdasdasdasdasdasd</a></li>
+                            <li><a href="#">asdasdasdasdasdasdasdasdasdasdasdasdasdasdasd</a></li>
+                            <li><a href="#">asdasdasdasdasdasdasdasdasdasdasdasdasdasdasd</a></li>
+                            <li><a href="#">asdasdasdasdasdasdasdasdasdasdasdasdasdasdasd</a></li>
+                            <li><a href="#">asdasdasdasdasdasdasdasdasdasdasdasdasdasdasd</a></li>
+                            <li><a href="#">asdasdasdasdasdasdasdasdasdasdasdasdasdasdasd</a></li>
+                        </ul>
+                    </div>
+                </aside>
+                <div class="map_content">
+fddf
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Index;
